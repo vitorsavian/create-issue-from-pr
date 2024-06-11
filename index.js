68e8b8aa6e3e5f3566d035ffa2180b1f0e2a797e
Vitor Savian <vitor.savian@suse.com>
2024-06-11 15:38:48 -0300

    build with vercel

--- index.js
+++ index.js
@@ -2,37 +2,41 @@ const core = require('@actions/core')
 const github = require('@actions/github')
 const { Octokit } = require('@ocktokit/rest')
 
-try {
-  const project = core.getInput('project');
-  const token = core.getInput('token');
-  const org = core.getInput('org');
-  const team = core.getInput('team');
-  const issueTitle = core.getInput('title');
-  const issueBody = core.getInput('body');
-  const user = core.getInput('user');
-  const octokit = new Octokit({ auth: token });
+async function run() {
+  try {
+    const project = core.getInput('project');
+    const token = core.getInput('token');
+    const org = core.getInput('org');
+    const team = core.getInput('team');
+    const issueTitle = core.getInput('title');
+    const issueBody = core.getInput('body');
+    const user = core.getInput('user');
+    const octokit = new Octokit({ auth: token });
 
-  const { data: teamMembers } = await octokit.teams.listMembersInOrg({
-    org: org,
-    team_slug: team
-  });
-
-  const userInTeam = teamMembers.some(member => member.login === user);
-
-  if (userInTeam) {
-    console.log(`${user} is in the team ${team}.`);
-  } else {
-    console.log(`${user} not in the team ${team}. Creating issue...`);
-
-    await octokit.issues.create({
-      owner: org,
-      repo: project,
-      title: issueTitle,
-      body: issueBody,
+    const { data: teamMembers } = await octokit.teams.listMembersInOrg({
+      org: org,
+      team_slug: team
     });
 
-    console.log(`Issue created.`);
+    const userInTeam = teamMembers.some(member => member.login === user);
+
+    if (userInTeam) {
+      console.log(`${user} is in the team ${team}.`);
+    } else {
+      console.log(`${user} not in the team ${team}. Creating issue...`);
+
+      await octokit.issues.create({
+        owner: org,
+        repo: project,
+        title: issueTitle,
+        body: issueBody,
+      });
+
+      console.log(`Issue created.`);
+    }
+  } catch (err) {
+    core.setFailed(err.message);
   }
-} catch (err) {
-  core.setFailed(err.message);
 }
+
+run()
