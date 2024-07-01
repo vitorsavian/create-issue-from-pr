9ea2cc33161aa79211ec36af6c161d4e9d5535ae
Vitor Savian <vitor.savian@suse.com>
2024-07-01 12:18:40 -0300

    New version 2.0 commit

--- index.js
+++ index.js
@@ -1,41 +1,75 @@
 import * as core from '@actions/core';
 import { Octokit } from "octokit";
 
-async function run() {
-  try {
-    const project = core.getInput('project');
-    const token = core.getInput('token');
-    const org = core.getInput('org');
-    const team = core.getInput('team');
-    const issueTitle = core.getInput('title');
-    const issueBody = core.getInput('body');
-    const user = core.getInput('user');
-    const octokit = new Octokit({ auth: token });
+class CreateIssueFromPR {
+  github
+  constructor(token) {
+    this.github = new Octokit({ auth: token })
+  }
 
-    const result = await octokit.request('GET /orgs/{org}/public_members/{username}', {
-      org: team,
-      username: user,
-      headers: {
-        'X-GitHub-Api-Version': '2022-11-28'
+  async checkMembership(org, user) {
+    try {
+      const result = await this.github.rest.orgs.checkPublicMembershipForUser({
+        org: org,
+        username: user,
+      })
+
+      return {
+        result: result,
+        err: undefined
       }
-    })
-
-    if (result.status == 204) {
-      console.log(`${user} is in the team ${team}.`);
-    } else {
-      console.log(`${user} not in the team ${team}. Creating issue...`);
-
-      await octokit.request('POST /repos/{owner}/{repo}/issues', {
-        owner: org,
-        repo: project,
-        title: issueTitle,
-        body: issueBody,
-      });
-
-      console.log(`Issue created.`);
+    } catch (err) {
+      return {
+        result: undefined,
+        err: err
+      }
     }
-  } catch (err) {
-    core.setFailed(err.message);
+  }
+
+  async createIssue(org, repo, title, body) {
+    try {
+      const result = await this.github.rest.issues.create({
+        owner: org,
+        repo: repo,
+        title: title,
+        body: body,
+      })
+
+      return {
+        result: result,
+        err: undefined
+      }
+    } catch (err) {
+      return {
+        result: undefined,
+        err: err
+      }
+    }
+  }
+}
+
+async function run() {
+  const repo = core.getInput('repo');
+  const token = core.getInput('token');
+  const org = core.getInput('org');
+  const title = core.getInput('title');
+  const body = core.getInput('body');
+  const user = core.getInput('user');
+
+  const github = new CreateIssueFromPR(token);
+
+  console.log(`Checking if the ${user} have membership in ${org}.`);
+  let { err } = await github.checkMembership(org, user);
+  if (err == undefined) {
+    return
+  }
+
+  if (err.status == 302 || err.status == 404) {
+    console.log(`${user} not found as a public member of the ${org}. Creating issue...`);
+    let { err } = await github.createIssue(org, repo, title, body)
+    if (err != undefined) {
+      core.setFailed(err.message)
+    }
   }
 }
 
